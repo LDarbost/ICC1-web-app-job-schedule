c2b0181f680d3b3c93f61b84619906d4b53597f7
LDarbost <Louisdarbost@gmail.com>
2025-08-15 13:53:07 +0100

    fix(Terraform) Chanegd name for entrypoint

--- terraform/terraforminit.sh
+++ terraform/terraforminit.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
 cd "$(dirname "$0")"
 
 # Terraform init with fallback
 terraform init -input=false -lockfile=readonly -upgrade || \
-  terraform init -input=false -reconfigure || { echo "Terraform init failed"; exit 1; }
+terraform init -input=false -reconfigure || { echo "Terraform init failed"; exit 1; }
 
 terraform providers
 
@@ -13,8 +13,8 @@ terraform providers
 terraform plan -out=tfplan
 
 # Only apply if _APPLY is "Y" or "y"
-if [[ "${1,,}" == "y" ]]; then
-  terraform apply -auto-approve tfplan
+if [ "$(echo "$1" | tr '[:upper:]' '[:lower:]')" = "y" ]; then
+    terraform apply -auto-approve tfplan
 else
-  echo "Skipping terraform apply (set _APPLY=Y to apply changes)"
+    echo "Skipping terraform apply (set _APPLY=Y to apply changes)"
 fi
