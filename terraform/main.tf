c97c67661329fd393b329ff5b1eea45330d7e223
LDarbost <Louisdarbost@gmail.com>
2025-08-15 11:57:11 +0100

    feat(Terraform)) Added PAT and conenction through installID

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,39 @@
+# GitHub Connection for 2nd Gen Repository
+resource "google_cloudbuild_v2_connection" "github_connection" {
+  project  = var.project_id
+  location = var.region
+  name     = "github-connection"
+
+  github_config {
+    app_installation_id = var.github_app_installation_id
+    authorizer_credential {
+      oauth_token_secret_version = var.github_oauth_token_secret
+    }
+  }
+}
+
+# Repository resource
+resource "google_cloudbuild_v2_repository" "repo" {
+  project           = var.project_id
+  location          = var.region
+  name              = var.repo_name
+  parent_connection = google_cloudbuild_v2_connection.github_connection.name
+  remote_uri        = "https://github.com/${var.github_owner}/${var.repo_name}.git"
+}
+
+# PR Trigger
 resource "google_cloudbuild_trigger" "cloud_run_pr_trigger" {
   name        = "cloud-run-pr-trigger"
   description = "Trigger for Cloud Run plan on pull request"
-  location = "europe-west1"
-  project = var.project_id
-  service_account = var.buildsaname
-  github {
-    owner = var.github_owner
-    name  = var.repo_name
+  location    = var.region
+  project     = var.project_id
+  
+  service_account = "projects/${var.project_id}/serviceAccounts/${var.buildsaname}"
+  
+  repository_event_config {
+    repository = google_cloudbuild_v2_repository.repo.id
+    
     pull_request {
-      invert_regex = false
       branch = "^main$"
     }
   }
@@ -16,22 +41,23 @@ resource "google_cloudbuild_trigger" "cloud_run_pr_trigger" {
   filename = "terraform/cloudbuild.yaml"
 
   substitutions = {
-    _APPLY      = "N"
+    _APPLY = "N"
   }
 }
 
+# Push Trigger
 resource "google_cloudbuild_trigger" "cloud_run_push_trigger" {
   name        = "cloud-run-push-trigger"
   description = "Trigger for Cloud Run apply on push request"
-  location = "europe-west1"
-  project = var.project_id
-  service_account = var.buildsaname
+  location    = var.region
+  project     = var.project_id
   
-  github {
-    owner = var.github_owner
-    name  = var.repo_name
+  service_account = "projects/${var.project_id}/serviceAccounts/${var.buildsaname}"
+  
+  repository_event_config {
+    repository = google_cloudbuild_v2_repository.repo.id
+    
     push {
-      invert_regex = false
       branch = "^main$"
     }
   }
@@ -39,31 +65,32 @@ resource "google_cloudbuild_trigger" "cloud_run_push_trigger" {
   filename = "terraform/cloudbuild.yaml"
 
   substitutions = {
-    _APPLY      = "Y"
+    _APPLY = "Y"
   }
 }
 
-resource "google_cloudbuild_trigger" "docker-tag-build" {
+# Tag Trigger
+resource "google_cloudbuild_trigger" "docker_tag_build" {
   name        = "docker-tag-build"
   description = "Trigger for tag based build for docker image"
-  location = "europe-west1"
-  project = var.project_id
-  service_account = var.buildsaname
+  location    = var.region
+  project     = var.project_id
+  
+  service_account = "projects/${var.project_id}/serviceAccounts/${var.buildsaname}"
 
-  github {
-    owner = var.github_owner
-    name  = var.repo_name
+  repository_event_config {
+    repository = google_cloudbuild_v2_repository.repo.id
+    
     push {
-      invert_regex = false
-      tag = "^v.*$"  # Matches tags starting with 'v'
+      tag = "^v.*$"
     }
   }
 
   filename = "terraform/dockerimage.yaml"
 
   substitutions = {
-    _APPLY      = "Y"
+    _APPLY           = "Y"
     _APPREGISTRYPATH = "europe-west1-docker.pkg.dev/hazel-sky-467411-j9/britedge-e1"
-    _IMAGE_NAME = "britedge-run"
+    _IMAGE_NAME      = "britedge-run"
   }
 }
