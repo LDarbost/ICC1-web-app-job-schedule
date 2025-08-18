81dfb3a7812be4214d54b1f8631538771c08b15e
LDarbost <Louisdarbost@gmail.com>
2025-08-18 18:41:39 +0100

    VPC Peering and Private IP

--- terraform/main.tf
+++ terraform/main.tf
@@ -178,5 +178,5 @@ resource "google_service_networking_connection" "private_vpc_connection" {
   network                 = google_compute_network.britedge-vpc.id
   service                 = "servicenetworking.googleapis.com"
   reserved_peering_ranges = [google_compute_global_address.private_ip_allocation.name]
-  depends_on = [google_compute_subnetwork.britedge_subnet]
+  depends_on = [google_compute_subnetwork.britedge-subnet]
 }
