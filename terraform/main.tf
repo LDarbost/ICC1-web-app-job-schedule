ef4f0edfce59ce7cceada2e06d762693cc4dd95a
LDarbost <Louisdarbost@gmail.com>
2025-08-19 00:42:18 +0100

    changed dockertag

--- terraform/main.tf
+++ terraform/main.tf
@@ -230,7 +230,6 @@ resource "google_sql_database" "britedge-database" {
 # Fetch the password from Secret Manager at Terraform apply time
 data "google_secret_manager_secret_version" "db_password" {
   secret  = "CloudSQL-user"
-  project = "488683797782"
 }
 
 # The actual database user, using the password from the data source
